227d8f9a17b06c85d0bed8ac8ca70cf85d90fe31
Kevin Nguyen <nguyenkevin225@gmail.com>
2020-05-15 14:47:12 -0700

    linked list practice

--- weekseven/linkedListTest.js
+++ weekseven/linkedListTest.js
@@ -1,15 +1,36 @@
+// reverse a linked list
 
-def reverse_list(self):
-    # TO BE COMPLETED
-    # if self.head.next_node is None:
-    #   return self.head 
-    previous = None
-    current = self.head
+function reverse_list(head){
+     if(head.next === null){
+          return head
+     }
 
-    while current != None:
-        new = current.next_node 
-        current.next_node = previous 
-        previous = current 
-        current = new
+     let previous = null 
+     let current = head 
 
-    self.head = previous
+     while(current !== null){
+          let newNode = current.next 
+          current.next = previous 
+          previous = current 
+          current = newNode
+     }
+
+     head = previous 
+     return head
+}
+
+
+// def reverse_list(self):
+//     # TO BE COMPLETED
+//     # if self.head.next_node is None:
+//     #   return self.head 
+//     previous = None
+//     current = self.head
+
+//     while current != None:
+//         new = current.next_node 
+//         current.next_node = previous 
+//         previous = current 
+//         current = new
+
+//     self.head = previous
